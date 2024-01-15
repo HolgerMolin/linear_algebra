c53d21a795ed4428541fd20ac5284ab18879d618
Holger <holger.molin@icloud.com>
2024-01-15 14:37:11 +0100

    Started testing suite, implemented matmul, tranpose, and map

--- matrix.py
+++ matrix.py
@@ -5,8 +5,20 @@ class MatrixShapeException(Exception):
         self.message = message
         super().__init__()
 
+def zero_matrix(shape: tuple[int]):
+    pass
+
+def one_matrix(shape: tuple[int]):
+    pass
+
+def rand_matrix(shape: tuple[int], random_type: str = "uniform" or "normal"):
+    pass
+
+def hadamard(A: Matrix, B:Matrix):
+    pass
+
 class Matrix:
-    def __init__(self, elements: list[list[float]] | list[list[int]]) -> None:
+    def __init__(self, elements: list[list[float]] | list[list[int]]):
         self.elements = elements
         self.shape = (len(elements), len(elements[0]))
 
@@ -19,7 +31,7 @@ class Matrix:
             for element_index, element in enumerate(column):
                 output_column.append(element + self.elements[column_index][element_index])
             output_matrix.append(output_column)
-        return output_matrix
+        return Matrix(output_matrix)
 
     def __mul__(self, factor: float | int | Matrix):
         if isinstance(factor, (float, int)):
@@ -29,7 +41,7 @@ class Matrix:
                 for element in column:
                     output_column.append(factor * element)
                 output_matrix.append(output_column)
-            return output_matrix
+            return Matrix(output_matrix)
 
         if isinstance(factor, Matrix):
             if self.shape[1] != factor.shape[0]:
@@ -38,7 +50,9 @@ class Matrix:
             for column_index in range(self.shape[0]):
                 output_column = []
                 for element_index in range(factor.shape[1]):
-                    output_column.append(sum())
+                    output_column.append(sum(self[column_index, i] * factor[i, element_index] for i in range(self.shape[1])))
+                output_matrix.append(output_column)
+            return Matrix(output_matrix)
 
     def __rmul__(self, factor: float | int):
         output_matrix = []
@@ -47,14 +61,37 @@ class Matrix:
             for element in column:
                 output_column.append(factor * element)
             output_matrix.append(output_column)
-        return output_matrix
+        return Matrix(output_matrix)
+    
+    def __getitem__(self, index: tuple(int, int)):
+        return self.elements[index[0]][index[1]]
+    
+    def __str__(self):
+        pass
 
     def transpose(self):
-        pass
+        output_matrix = []
+        for column_index in range(self.shape[1]):
+            output_column = []
+            for element_index in range(self.shape[0]):
+                output_column.append(self[element_index, column_index])
+            output_matrix.append(output_column)
+        return Matrix(output_matrix)
 
     def trace(self):
         if self.shape[0] != self.shape[1]:
             raise MatrixShapeException("Matrix is not square")
         return sum(self.elements[i][i] for i in range(self.shape[0]))
+    
+    def map(self, function):
+        output_matrix = []
+        for column in self.elements:
+            output_column = []
+            for element in column:
+                output_column.append(function(element))
+            output_matrix.append(output_column)
+        return Matrix(output_matrix)
+
+    
         
             
--- testing.py
+++ testing.py
@@ -1,15 +1,81 @@
 from matrix import Matrix 
+import unittest
+import torch
+from random import uniform
 
-mat1_list = [[1, 2], [1, 2]]
-mat2_list = [[-1, -2], [-1, -2]]
+def tensor_to_matrix(tensor: torch.Tensor) -> Matrix:
+    if tensor.dim() != 2:
+        raise ValueError("Input tensor must be a 2D tensor")
 
-mat1 = Matrix(mat1_list)
-mat2 = Matrix(mat2_list)
+    elements = tensor.tolist()
+    return Matrix(elements)
 
-print(mat1.trace())
+NUM_SAMPLES = 10
 
-#TODO
-# Make sure that product shapes are correct
+class MatrixTests(unittest.TestCase):
+    def test_matrix_trace(self):
+        shapes = [(i, i) for i in range(1,10)]
+        passed = True
+        for shape in shapes:
+            for _ in range(NUM_SAMPLES):
+                A_torch = torch.rand(shape)
+                A_mat = tensor_to_matrix(A_torch)
+                torch_trace = A_torch.trace().item()
+                matrix_trace = A_mat.trace()
+                self.assertAlmostEqual(torch_trace, matrix_trace, delta=1e-6)
+
+        print("Passed matrix trace test")
+
+    def test_matrix_addition(self):
+        shapes = [(i, j) for j in range(1, 10) for i in range(1, 10)]
+        for shape in shapes:
+            for _ in range(NUM_SAMPLES):
+                A_torch = torch.rand(shape)
+                B_torch = torch.rand(shape)
+                A_mat = tensor_to_matrix(A_torch)
+                B_mat = tensor_to_matrix(B_torch)
+
+                C_torch = A_torch + B_torch
+                C_mat = A_mat + B_mat
+                for i in range(shape[0]):
+                    for j in range(shape[1]):
+                        self.assertAlmostEqual(C_torch[i, j].item(), C_mat[i, j], delta=1e-6)
+
+        print("Passed matrix addition test")
+    def test_matrix_scalar_multiplication(self):
+        shapes = [(i, j) for j in range(1, 10) for i in range(1, 10)]
+        for shape in shapes:
+            for _ in range(NUM_SAMPLES):
+                A_torch = torch.rand(shape)
+                A_mat = tensor_to_matrix(A_torch)
+                alpha = uniform(-100.0, 100.0)
+
+                A_torch = alpha * A_torch
+                A_mat = alpha * A_mat
+
+                for i in range(shape[0]):
+                    for j in range(shape[1]):
+                        self.assertAlmostEqual(A_torch[i, j].item(), A_mat[i, j], delta=1e-5)
+        print("Passed matrix scalar multiplication test")
+
+    def test_matrix_multiplication(self):
+        shapes = [(i, j, k) for i in range(1, 10) for j in range(1, 10) for k in range(1, 10)]
+        for shape in shapes:
+            for _ in range(NUM_SAMPLES):
+                A_torch = torch.rand((shape[0], shape[1]))
+                A_mat = tensor_to_matrix(A_torch)
+                B_torch = torch.rand((shape[1], shape[2]))
+                B_mat = tensor_to_matrix(B_torch)
+                C_torch = torch.matmul(A_torch, B_torch)
+                C_mat = A_mat * B_mat
+                for i in range(C_mat.shape[0]):
+                    for j in range(C_mat.shape[1]):
+                        self.assertAlmostEqual(C_torch[i, j].item(), C_mat[i, j], delta=1e-6)
+        print("Passed matrix multiplication test")
 
 
-
+test = MatrixTests()
+test.test_matrix_trace()
+test.test_matrix_addition()
+test.test_matrix_scalar_multiplication()
+test.test_matrix_multiplication()
